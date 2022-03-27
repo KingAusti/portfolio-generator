58228b3daf5fd8fba1cbe8c9da647ccbad33d4b5
KingAusti <austin@thehenrys.xyz>
2022-03-26 22:14:40 -0700

    adding generatePage function to dynamically create html github repo

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
 
-const printProfileData = profileDataArr => {
-    profileDataArr.forEach((profileItem) => console.log(profileItem));
-};
+const generatePage = require('./src/page-template');
+
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+const pageHTML = generatePage(name, github);
+
+fs.writeFile('./index.html', pageHTML, err => {
+    if (err) throw new Error(err);
+
+    console.log('Portfolio Complete! Checkout the index.html to see the output!')
+
+});
+
+// 
+// console.log(profileDataArgs);
+// const printProfileData = profileDataArr => {
+//     profileDataArr.forEach((profileItem) => console.log(profileItem));
+// };
